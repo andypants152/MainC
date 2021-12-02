c60b0cba34eecc6b57e17d08f77c15acd79f5400
AndyPants <andypants152@gmail.com>
2021-12-01 21:13:18 -0800

    added some comments to hello world?

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 
 void main(){
+    /**
+     * Including stdio.h and using functions such as printf() 
+     * will use a large number of the background tiles for font characters. 
+     * If stdio.h is not included then that space will be available for use with other tiles instead.
+     * 
+     * You'll already lose 19 Tiles of vram to get the nintendo logo which is required to boot on official hardware
+     * though i suppose you can overwrite those tiles once booted
+     * 
+     * seeing as the vram viewer has 00:7F tiles you'll have 8*16*6 tiles of vram, which is 768 tiles of 8*8 pixel tiles
+     * oh, 768 is for color mode with double the vram... anyway i don't know what i'm doing so pressing onward!
+     * 
+     * I'd have said we should load our own font here, but then someone would come along and say why load all the alphabet too..
+     * 
+     **/
     printf("HELLO WORLD");
 
 }
